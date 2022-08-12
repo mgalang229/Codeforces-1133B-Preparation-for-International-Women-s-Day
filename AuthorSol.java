42e8bd10d0012cb256bc863aa51c35d767451f00
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2022-08-12 21:48:16 +0800

    Create AuthorSol.java

---
+++ AuthorSol.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class AuthorSol {
+	
+	public static void main(String[] args) {	
+		FastScanner fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int T = 1;
+		//T = fs.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int n = fs.nextInt();
+			int k = fs.nextInt();
+			int[] counter = new int[k];
+			for (int i = 0; i < n; i++) {
+				int x = fs.nextInt();
+				counter[x%k]++;
+			}
+			int ans = counter[0] / 2;
+			if (k % 2 == 0) {
+				ans += counter[k/2]/2;
+			}
+			for (int i = 1; i < (k + 1) / 2; i++) {
+				int j = k - i;
+				ans += Math.min(counter[i], counter[j]);
+			}
+			out.println(ans * 2);
+		}
+		out.close();
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		for (int x : a) {
+			arr.add(x);
+		}
+		Collections.sort(arr);
+		for (int i = 0; i < a.length; i++) {
+			a[i] = arr.get(i);
+		}
+	}
+	
+	static void swap(int[] a, int x, int y) {
+		int temp = a[x];
+		a[x] = a[y];
+		a[y] = temp;
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while (!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextInt();
+			}
+			return a;
+		}
+		
+		long[] readLongArray(int n) {
+			long[] a = new long[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextLong();
+			}
+			return a;
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
